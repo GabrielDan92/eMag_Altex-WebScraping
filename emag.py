875be02ac4931a696f3f70c0fcb435d6d86f8ea1
Gabriel <gaby.tzu@windowslive.com>
2020-09-17 10:53:36 +0300

    Update emag.py

--- emag.py
+++ emag.py
@@ -1,6 +1,8 @@
+import os
 import re
-import requests
+import ctypes
 import pandas
+import requests
 import threading
 import requests_random_user_agent
 from bs4 import BeautifulSoup
@@ -13,9 +15,17 @@ productsPriceII = []
 productsPriceOld = []
 productsPriceOldII = []
 productLink = []
-url = "https://www.emag.ro/jocuri-consola-pc"
-url = "https://www.emag.ro/placi_video"
-url = "https://www.emag.ro/solid-state_drive_ssd_"
+os.chdir("C:\\Users\\gabyt\\Desktop")
+
+while True:
+    userInput = input("Adauga link-ul eMag aici: ")
+    if userInput != "":
+        break
+
+url = userInput
+# url = "https://www.emag.ro/jocuri-consola-pc"
+# url = "https://www.emag.ro/placi_video"
+# url = "https://www.emag.ro/solid-state_drive_ssd_"
 
 def emag(i):
     global productsTitle
@@ -47,60 +57,64 @@ def emag(i):
 
 
 # find the page count
-url += "/c"
-user_agent = str(requests.get('https://httpbin.org/user-agent'))
-data = requests.get(url=url, headers={"user-agent": user_agent})
-soup = BeautifulSoup(data.text, "html.parser")
-count = soup.find("span", {"class": "visible-xs"})
-count = re.findall(r'((?<=\">1 din ).*([^\r]*)(?=</span>))', str(count))
-count = str(count[0]).strip("('")
-count = int(count.strip("', '')"))
+try:
+    url += "/c"
+    user_agent = str(requests.get('https://httpbin.org/user-agent'))
+    data = requests.get(url=url, headers={"user-agent": user_agent})
+    soup = BeautifulSoup(data.text, "html.parser")
+    pageCount = soup.find("span", {"class": "visible-xs"})
+    pageCount = re.findall(r'((?<=\">1 din ).*([^\r]*)(?=</span>))', str(pageCount))
+    pageCount = str(pageCount[0]).strip("('")
+    pageCount = int(pageCount.strip("', '')"))
 
-# generate threads for each page
-threads = []
-for i in range(1, count + 1):
-    threadObj = threading.Thread(target=emag, args=(i,))
-    threads.append(threadObj)
-y = [x.start() for x in threads]
-y = [x.join() for x in threads]
+    # generate threads for each page
+    threads = []
+    for i in range(1, pageCount + 1):
+        threadObj = threading.Thread(target=emag, args=(i,))
+        threads.append(threadObj)
+    y = [x.start() for x in threads]
+    y = [x.join() for x in threads]
 
-# clean the retrieved data
-badChars = [
-    ('\[\(\'', ''),
-    ('\,\)\]', ''),
-    ('\[\]', ''),
-    ('\[\'', ''),
-    ('\'', ''),
-    ('/\"', ''),
-    (', \)\]', ''),
-    ('\]', ''),
-    ('\&amp\;#039\;', '\'')
-]
+    # clean the retrieved data
+    badChars = [
+        ('\[\(\'', ''),
+        ('\,\)\]', ''),
+        ('\[\]', ''),
+        ('\[\'', ''),
+        ('\'', ''),
+        ('/\"', ''),
+        (', \)\]', ''),
+        ('\]', ''),
+        ('\&amp\;#039\;', '\'')
+    ]
 
-for old, new in badChars:
-    productLink[:] = [re.sub(old, new, str(x)) for x in productLink]
-    productsTitle[:] = [re.sub(old, new, str(x)) for x in productsTitle]
-    productsPrice[:] = [re.sub(old, new, str(x)) for x in productsPrice]
-    productsPriceII[:] = [re.sub(old, new, str(x)) for x in productsPriceII]
-    productsPriceOld[:] = [re.sub(old, new, str(x))  for x in productsPriceOld]
-    productsPriceOldII[:] = [re.sub(old, new, str(x))  for x in productsPriceOldII]
+    for old, new in badChars:
+        productLink[:] = [re.sub(old, new, str(x)) for x in productLink]
+        productsTitle[:] = [re.sub(old, new, str(x)) for x in productsTitle]
+        productsPrice[:] = [re.sub(old, new, str(x)) for x in productsPrice]
+        productsPriceII[:] = [re.sub(old, new, str(x)) for x in productsPriceII]
+        productsPriceOld[:] = [re.sub(old, new, str(x))  for x in productsPriceOld]
+        productsPriceOldII[:] = [re.sub(old, new, str(x))  for x in productsPriceOldII]
 
-productsTitle[:] = [str(x).split(",") for x in productsTitle]
-productsTitle[:] = [x[0] for x in productsTitle]
-productLink[:] = [str(x).split(",") for x in productLink]
-productLink[:] = [x[0] for x in productLink]
+    productsTitle[:] = [str(x).split(",") for x in productsTitle]
+    productsTitle[:] = [x[0] for x in productsTitle]
+    productLink[:] = [str(x).split(",") for x in productLink]
+    productLink[:] = [x[0] for x in productLink]
 
-df = pandas.DataFrame({"Titlu": productsTitle, "productsPrice": productsPrice, "productsPriceII": productsPriceII, "productsPriceOld": productsPriceOld, "productsPriceOldII": productsPriceOldII, "Link": productLink})
+    df = pandas.DataFrame({"Titlu": productsTitle, "productsPrice": productsPrice, "productsPriceII": productsPriceII, "productsPriceOld": productsPriceOld, "productsPriceOldII": productsPriceOldII, "Link": productLink})
 
-# concat the two temp price columns
-df["Pret Final"] = df["productsPrice"].astype(str)+ "," + df["productsPriceII"].astype(str)
-df["Pret Original"] = df["productsPriceOld"].astype(str)+ "," + df["productsPriceOldII"].astype(str)
+    # concat the two temp price columns
+    df["Pret Final"] = df["productsPrice"].astype(str)+ "," + df["productsPriceII"].astype(str)
+    df["Pret Original"] = df["productsPriceOld"].astype(str)+ "," + df["productsPriceOldII"].astype(str)
 
-# replace empty values "," with blank values
-df["Pret Original"][df["Pret Original"] == ","] = ""
+    # replace empty values "," with blank values
+    df["Pret Original"][df["Pret Original"] == ","] = ""
 
-# drop empty columns and reassign the columns to the dataframe
-df = df[["Titlu", "Pret Original", "Pret Final", "Link"]]
-df = df[df.Titlu != '']
-df.set_index("Titlu", inplace=True)
-df.to_excel(str(date.today().strftime("%b-%d-%Y")) + "_eMag.xlsx")
+    # drop empty columns and reassign the columns to the dataframe
+    df = df[["Titlu", "Pret Original", "Pret Final", "Link"]]
+    df = df[df.Titlu != '']
+    df.set_index("Titlu", inplace=True)
+    df.to_excel(str(date.today().strftime("%b-%d-%Y")) + "_eMag.xlsx")
+    ctypes.windll.user32.MessageBoxW(0, "Preturile au fost extrase.", "Warning", 0)
+except:
+    ctypes.windll.user32.MessageBoxW(0, "Link-ul nu este valid. Te rog incearca din nou.", "Warning", 0)
